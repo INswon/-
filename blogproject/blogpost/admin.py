721a3869b36d3733c827e54605660b0984e0589e
INswon <152695318+INswon@users.noreply.github.com>
2023-12-09 23:10:33 +0900

    Delete blogproject directory

--- blogproject/blogpost/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import SampleModel,BlogModel, Category
-
-
-admin.site.register(SampleModel)
-admin.site.register(BlogModel)
-admin.site.register(Category)
--- blogproject/blogpost/context.py
+++
@@ -1,7 +0,0 @@
-from .models import Category
-
-def related(request):
-    context = {
-        'category_list': Category.objects.all(),
-    }
-    return context
